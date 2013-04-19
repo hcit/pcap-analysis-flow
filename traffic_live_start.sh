a4f609d1d928ef9a3dd4104911d7e79ad64c98eb
front <front@R610-NP-ubuntu.(none)>
2013-04-19 17:44:49 +0800

    Debug

--- traffic_live_start.sh
+++ traffic_live_start.sh
@@ -6,8 +6,8 @@ datetime=`date '+%Y%m%d-%H%M%S'`
 
 justniffer -i eth1 -F -c $scripthome/justniffer/justniffer-light.conf > wifi_justniffer_$datetime &
 
-tstat -i eth1 -l -N pcap-analysis-flow/tstat.netconf -s wifi_tstat_$datetime &
+tstat -i eth1 -l -N $scripthome/tstat.netconf -s wifi_tstat_$datetime &
 
-$scripthome/pcapDPI/pcapReader -i eth1 -w wifi_dpi_$datetime &
+#$scripthome/pcapDPI/pcapReader -i eth1 -w wifi_dpi_$datetime &
 
 exit 0;
