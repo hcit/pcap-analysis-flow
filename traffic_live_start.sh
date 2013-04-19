b0be24cf64bd460416a3f321ead8509ad67ab632
Jamin <chenxm35@gmail.com>
2013-04-19 16:05:26 +0800

    add live script

---
+++ traffic_live_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+scripthome=`dirname $0`
+
+datetime=`date '+%Y%m%d-%H%M%S'`
+
+justniffer -i eth1 -F -c $scripthome/justniffer/justniffer-light.conf > wifi_justniffer_$datetime &
+
+tstat -i eth1 -l -N pcap-analysis-flow/tstat.netconf -s wifi_tstat_$datetime &
+
+$scripthome/pcapDPI/pcapReader -i eth1 -w wifi_dpi_$datetime &
+
+exit 0;
