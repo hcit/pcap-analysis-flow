25b80090a4ac497a99273aa3432f53557eecfa5f
root <root@R610-NP-ubuntu.(none)>
2013-04-20 16:44:55 +0800

    debug

--- traffic_live_start.sh
+++ traffic_live_start.sh
@@ -5,7 +5,7 @@ trap '[[ $pid ]] && kill $pid && kill_processes' 1 2 3 6 9 15
 kill_processes(){
         kill -9 `cat $justpid`
         kill -2 `cat $tstatpid`
-        kill -2 `cat $dpipid`
+        #kill -2 `cat $dpipid`
 }
 
 scripthome=`dirname $0`
@@ -20,8 +20,8 @@ echo $! > $justpid
 tstat -i eth1 -l -N $scripthome/tstat.netconf -s wifi_tstat_$datetime &
 echo $! > $tstatpid
 
-$scripthome/pcapDPI/pcapReader -i eth1 -w wifi_dpi_$datetime &
-echo $! > $dpipid
+#$scripthome/pcapDPI/pcapReader -i eth1 -w wifi_dpi_$datetime &
+#echo $! > $dpipid
 
 sleep 12h & pid=$!
 wait
