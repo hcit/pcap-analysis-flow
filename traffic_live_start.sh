b1c8e7bfc77645cb89e754a34c9f38398e86e18f
root <root@R610-NP-ubuntu.(none)>
2013-04-22 13:08:04 +0800

    debug live traffic

--- traffic_live_start.sh
+++ traffic_live_start.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
-pid=
-trap '[[ $pid ]] && kill $pid && kill_processes' 1 2 3 6 9 15
+export PATH=/usr/loca/bin:/usr/bin:/bin:$PATH
 
 kill_processes(){
         kill -9 `cat $justpid`
         kill -2 `cat $tstatpid`
-        #kill -2 `cat $dpipid`
+        kill -2 `cat $dpipid`
 }
 
 scripthome=`dirname $0`
 justpid="/var/run/mypid-just"
 tstatpid="/var/run/mypid-tstat"
 dpipid="/var/run/mypid-dpi"
-datetime=`date '+%Y%m%d-%H%M%S'`
+datetime=`date '+%Y%m%d-%H%M'`
 
 ## Parse options
 if [ $# -ne 1 ]; then
         echo "Usage: $0 <outputfolder>" && exit 1;
 else
         outfolder=$1
-        mkdir -p $outfolder
 fi
 
-justniffer -i eth1 -F -c $scripthome/justniffer/justniffer-light.conf > $outfolder/wifi_justniffer_$datetime &
-echo $! > $justpid
-
-tstat -i eth1 -l -N $scripthome/tstat.netconf -s $outfolder/wifi_tstat_$datetime &
+mkdir -p $outfolder
+/usr/local/bin/tstat -i eth1 -l -N $scripthome/tstat.netconf -s $outfolder/wifilog_tstat &
 echo $! > $tstatpid
 
-#$scripthome/pcapDPI/pcapReader -i eth1 -w $outfolder/wifi_dpi_$datetime &
-#echo $! > $dpipid
+mkdir -p $outfolder/wifilog_justniffer
+/usr/bin/justniffer -i eth1 -F -c $scripthome/justniffer/justniffer-light.conf > $outfolder/wifilog_justniffer/$datetime.jn.out &
+echo $! > $justpid
 
-sleep 12h & pid=$!
+mkdir -p $outfolder/wifilog_ndpi
+$scripthome/pcapDPI/pcapReader -i eth1 -w $outfolder/wifilog_ndpi/$datetime.dpi.out &
+echo $! > $dpipid
+
+pid=
+trap '[[ $pid ]] && kill $pid && kill_processes' 1 2 3 6 9 15
+sleep 58m & pid=$!
 wait
 
 kill_processes
